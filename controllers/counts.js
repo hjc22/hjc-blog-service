de68e9e0aaf9fddedfdc7e633eddb2b4412e982c
huangjingchao <huangjingchao-cf@matorsc.com>
2018-04-04 17:51:10 +0800

    Up

--- controllers/counts.js
+++ controllers/counts.js
@@ -6,9 +6,17 @@ let counts = async (ctx) => {
 
   try {
 
-     const [,counts] = await getCollection('counts')
+     const [db,counts] = await getCollection('counts'),[,tags] = await getCollection('tags',db),[,comments] = await getCollection('comments',db),[,articles] = await getCollection('articles',db)
 
-     const list = await counts.find({},{projection:{_id:0}}).toArray()
+     const list = await counts.find({},{projection:{_id:0}}).toArray(),tagCount = await tags.count(),commentCount = await comments.count(),articleCount = await articles.count()
+
+     list[0].num = tagCount
+
+     list[2].num = 7
+
+     list[3].num = articleCount
+
+     list[4].num = commentCount
 
      ctx.body = resolve(list)
 
